6bcebc1a7e5d68425a9f8cbf718cf983435f50ca
annlumia <ann.lumia@live.com>
2020-02-26 09:35:47 +0700

    add vuex & router

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ const path = require('path')
 
 module.exports = {
   outputDir: 'build/www',
-  transpileDependencies: ['vuetify'],
-  transpileDependencies: ['vue-echarts', 'resize-detector'],
+  // transpileDependencies: ['vue-echarts', 'resize-detector'],
   chainWebpack: config => {
     config
       .entry('app')
